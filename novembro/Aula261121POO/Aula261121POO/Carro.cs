6fea9b41b6055aa771b7669a44ffef3e315757d2
Vinicius Nascimento <83601645+viniciusnasc@users.noreply.github.com>
2021-11-26 21:06:12 -0300

    Aula POO

--- novembro/Aula261121POO/Aula261121POO/Carro.cs
+++ novembro/Aula261121POO/Aula261121POO/Carro.cs
@@ -8,56 +8,56 @@ namespace Aula261121POO
 {
     class Carro
     {
-        public string Modelo { get; private set; }
-        public string Cor { get; private set; }
-        public int AnoMarca { get; private set; }
-        public int Chassi { get; private set; }
-        public string Proprietario { get; private set; }
-        public double VelocidadeMax { get; private set; }
-        public double VelocidadeAtual { get; private set; }
-        public int NumPortas { get; private set; }
-        public bool TetoSolar { get; private set; }
-        public int NumMarcha { get; private set; }
-        public bool CambioAutomatico { get; private set; }
-        public double VolumeCombustivel { get; private set; }
+        private string _modelo;
+        private string _cor;
+        private int _anoMarca;
+        private int _chassi;
+        private string _proprietario;
+        private double _velocidadeMax;
+        private double _velocidadeAtual;
+        private int _numPortas;
+        private bool _tetoSolar;
+        private int _numMarcha;
+        private bool _cambioAutomatico;
+        private double _volumeCombustivel;
 
         public Carro(string modelo, string cor, int anoMarca, int chassi,
                      string proprietario, double velocidadeMax, double velocidadeAtual, int numPortas,
                      bool tetoSolar, int numMarcha, bool cambioAutomatico, double volumeCombustivel)
         {
-            Modelo = modelo;
-            Cor = cor;
-            AnoMarca = anoMarca;
-            Chassi = chassi;
-            Proprietario = proprietario;
-            VelocidadeMax = velocidadeMax;
-            VelocidadeAtual = velocidadeAtual;
-            NumPortas = numPortas;
-            TetoSolar = tetoSolar;
-            NumMarcha = numMarcha;
-            CambioAutomatico = cambioAutomatico;
-            VolumeCombustivel = volumeCombustivel;
+            _modelo = modelo;
+            _cor = cor;
+            _anoMarca = anoMarca;
+            _chassi = chassi;
+            _proprietario = proprietario;
+            _velocidadeMax = velocidadeMax;
+            _velocidadeAtual = velocidadeAtual;
+            _numPortas = numPortas;
+            _tetoSolar = tetoSolar;
+            _numMarcha = numMarcha;
+            _cambioAutomatico = cambioAutomatico;
+            _volumeCombustivel = volumeCombustivel;
         }
 
         public void Acelerar()
         {
-            if (VelocidadeAtual < VelocidadeMax)
-                VelocidadeAtual++;
+            if (_velocidadeAtual < _velocidadeMax)
+                _velocidadeAtual++;
         }
 
         public void Freia()
         {
-            VelocidadeAtual = 0;
+            _velocidadeAtual = 0;
         }
 
         public void TrocaMarcha()
         {
-            NumMarcha++;
+            _numMarcha++;
         }
 
         public void ReduzMarcha()
         {
-            NumMarcha--;
+            _numMarcha--;
         }
     }
 }
